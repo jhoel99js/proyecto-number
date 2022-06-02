8d2ba7986405d4e508c6057543a83bc9037799c7
Claus Chocho <clauschochoe@gmail.com>
2022-06-01 20:56:42 -0500

    change styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,61 +2,51 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../Styles/Home.css";
 
-
 export const HomePage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  const [formValues, setFormValues] = useState({
+    name: "",
+  });
 
-    const [formValues, setFormValues] = useState({
-        name: "",
+  const { name } = formValues;
 
-    });
+  const handlerSubmit = (event) => {
+    event.preventDefault();
+    console.log("Datos ", formValues);
+    if (formValues.name === "") {
+      navigate("/NumberPage");
+    } else {
+      alert("Error", "Ingrese Su Nombre", "error");
+    }
+  };
 
-    const { name } = formValues;
+  const handlerOnChange = ({ target }) => {
+    const { name, value } = target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-    const handlerSubmit = (event) => {
-        event.preventDefault();
-        console.log("Datos ", formValues);
-        if (
-            formValues.name === ""
-        ) {
-            navigate("/NumberPage");
-        } else {
-            alert("Error", "Ingrese Su Nombre", "error");
-        }
-    };
-
-    const handlerOnChange = ({ target }) => {
-        const { name, value } = target;
-        setFormValues({ ...formValues, [name]: value });
-    };
-
-    return (
-        
-        <div className="text-center">
-            
-            <div className="form-signin w-100 m-auto home_container">
-                <h1>Bienvenido</h1>
-                <div>
-                   
-
-                    <form onSubmit={handlerSubmit}>
-                         
-                    <h1 class="h3 mb-3 fw-normal">Listo para aprender! </h1>
-                        <div className="form-floating " > 
-                            <input id="name"
-                                className="form-control"
-                                onChange={handlerOnChange} />
-                            <label for="floatingInput"  >
-                                Nombre
-                            </label>
-                        </div>
-                        <button type="submit" className="w-100 btn btn-lg btn-primary">
-                            Jugar
-                        </button>
-                    </form>
-                </div>
+  return (
+    <div className="text-center vh-100 vw-100">
+      <div className="form-signin w-100 m-auto home_container">
+        <h1>Bienvenido</h1>
+        <div>
+          <form onSubmit={handlerSubmit}>
+            <h1 class="h3 mb-3 fw-normal">Listo para aprender! </h1>
+            <div className="form-floating mb-3">
+              <input
+                id="name"
+                className="form-control"
+                onChange={handlerOnChange}
+              />
+              <label for="floatingInput">Nombre</label>
             </div>
+            <button type="submit" className="w-100 btn btn-lg btn-primary">
+              Jugar
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
